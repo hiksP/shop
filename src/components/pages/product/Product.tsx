8c4b8c80baa1ba4496a31a6c9fbde3b1737a57c8
Plavskikh Peter <qksyo@yandex.ru>
2022-11-22 00:28:48 +0700

    feat: button component

--- src/components/pages/product/Product.tsx
+++ src/components/pages/product/Product.tsx
@@ -4,6 +4,7 @@ import { ProductService } from "../../../services/ProductService";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import Loader from "../ui/loader/Loader";
+import Button from "../ui/Button/Button";
 
 const Product: FC = () => {
   const params = useParams();
@@ -12,7 +13,13 @@ const Product: FC = () => {
     ProductService.getProductById(params.id || "")
   );
 
-  return <Layout>{isLoading ? <Loader></Loader> : clothing?.name}</Layout>;
+  return (
+    <Layout>
+      {isLoading && <Loader></Loader>}
+      {clothing?.name}
+      <Button>Add to cart</Button>
+    </Layout>
+  );
 };
 
 export default Product;
